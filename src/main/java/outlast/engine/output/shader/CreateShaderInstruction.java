0cbfba5ea7e0e8af7e0aae284b7ca534514667ff
Ryan Plessner <rpless@ccs.neu.edu>
2013-08-21 18:28:37 -0400

    moved shader program creation logic to the CreateShaderInstruction

--- src/main/java/outlast/engine/output/shader/CreateShaderInstruction.java
+++ src/main/java/outlast/engine/output/shader/CreateShaderInstruction.java
@@ -1,13 +1,14 @@
 package outlast.engine.output.shader;
 
+import com.jogamp.common.nio.Buffers;
 import outlast.engine.output.Asset;
 import outlast.engine.output.JOGLInstruction;
 
 import javax.media.opengl.GL3;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CreateShaderInstruction extends JOGLInstruction<GL3> {
 
@@ -19,7 +20,7 @@ public class CreateShaderInstruction extends JOGLInstruction<GL3> {
         shaders = new ArrayList<>();
     }
 
-    /* Creational Methods */
+    /* Builder Methods */
 
     public CreateShaderInstruction with(Shader...shaders) {
         this.shaders.addAll(Arrays.asList(shaders));
@@ -40,6 +41,79 @@ public class CreateShaderInstruction extends JOGLInstruction<GL3> {
 
     @Override
     public void render(GL3 gl) {
-        shaderAsset.getValue().createProgram(gl, shaders);
+        createProgram(gl, shaders);
+    }
+
+    void createProgram(GL3 gl, List<Shader> shaders) {
+        for (Shader shader : shaders) {
+            shader.compile(gl);
+        }
+        int handle = gl.glCreateProgram();
+        for (Shader shader : shaders) {
+            gl.glAttachShader(handle, shader.getHandle());
+        }
+        linkPrograms(gl, handle);
+        for (Shader shader : shaders) {
+            gl.glDetachShader(handle, shader.getHandle());
+        }
+        shaderAsset.getValue().setHandle(handle);
+        getUniforms(gl, handle);
+        getAttributes(gl, handle);
+    }
+
+    private void linkPrograms(GL3 gl, int handle) {
+        gl.glLinkProgram(handle);
+        if(!isLinked(gl, handle)) {
+            printLinkerError(gl, handle);
+        }
+    }
+
+    private void printLinkerError(GL3 gl, int handle) {
+        IntBuffer logLengthBuffer = Buffers.newDirectIntBuffer(1);
+        gl.glGetProgramiv(handle, GL3.GL_INFO_LOG_LENGTH, logLengthBuffer);
+        int length = logLengthBuffer.get();
+
+        ByteBuffer data = Buffers.newDirectByteBuffer(length);
+        gl.glGetProgramInfoLog(handle, length, null, data);
+        byte[] bytes = new byte[length];
+        data.get(bytes);
+        System.out.println(new String(bytes));
+    }
+
+    private boolean isLinked(GL3 gl, int handle) {
+        IntBuffer status = Buffers.newDirectIntBuffer(1);
+        gl.glGetProgramiv(handle, GL3.GL_INFO_LOG_LENGTH, status);
+        return status.get() == GL3.GL_TRUE;
+    }
+
+    private void getUniforms(GL3 gl, int handle) {
+        IntBuffer countBuffer = IntBuffer.allocate(1);
+        gl.glGetProgramiv(handle, GL3.GL_ACTIVE_UNIFORMS, countBuffer);
+        int limit = countBuffer.get();
+        for(int i = 0; i < limit; i++) {
+            IntBuffer length = IntBuffer.allocate(1);
+            ByteBuffer buffer = ByteBuffer.allocate(100);
+            gl.glGetActiveUniformName(handle, i, 100, length, buffer);
+            byte[] bytearr = new byte[buffer.remaining()];
+            buffer.get(bytearr);
+            String name = new String(bytearr).trim();
+            shaderAsset.getValue().addUniform(name, gl.glGetUniformLocation(handle, name));
+        }
+    }
+
+    private void getAttributes(GL3 gl, int handle) {
+        IntBuffer countBuffer = IntBuffer.allocate(1);
+        gl.glGetProgramiv(handle, GL3.GL_ACTIVE_ATTRIBUTES, countBuffer);
+        int limit = countBuffer.get();
+        for(int i = 0; i < limit; i++) {
+            IntBuffer length = IntBuffer.allocate(1);
+            IntBuffer size = IntBuffer.allocate(1);
+            ByteBuffer buffer = ByteBuffer.allocate(100);
+            gl.glGetActiveAttrib(handle, i, 100, length, IntBuffer.allocate(1), IntBuffer.allocate(1), buffer);
+            byte[] bytearr = new byte[buffer.remaining()];
+            buffer.get(bytearr);
+            String name = new String(bytearr).trim();
+            shaderAsset.getValue().addAttribute(name, gl.glGetAttribLocation(handle, name));
+        }
     }
 }
--- src/main/java/outlast/engine/output/shader/ShaderProgram.java
+++ src/main/java/outlast/engine/output/shader/ShaderProgram.java
@@ -18,8 +18,8 @@ public class ShaderProgram extends NativeObject {
         return new CreateShaderInstruction(asset);
     }
 
-    public Map<String, Integer> uniforms;
-    public Map<String, Integer> attributes;
+    private Map<String, Integer> uniforms = new HashMap<>();
+    private Map<String, Integer> attributes = new HashMap<>();
 
     public void useProgram(GL3 gl) {
         gl.glUseProgram(getHandle());
@@ -37,88 +37,11 @@ public class ShaderProgram extends NativeObject {
         return attributes.get(name);
     }
 
-    void createProgram(GL3 gl, List<Shader> shaders) {
-        if(getHandle() < 0) {
-            for(Shader shader : shaders) {
-                shader.compile(gl);
-            }
-            int handle = gl.glCreateProgram();
-            for(Shader shader : shaders) {
-                gl.glAttachShader(handle, shader.getHandle());
-            }
-            linkPrograms(gl, handle);
-            for(Shader shader : shaders) {
-                gl.glDetachShader(handle, shader.getHandle());
-            }
-            this.setHandle(handle);
-            uniforms = getUniforms(gl);
-            attributes = getAttrbutes(gl);
-        }
+    void addAttribute(String name, Integer location) {
+        attributes.put(name, location);
     }
 
-    void createProgram(GL3 gl, Shader...shaders) {
-        createProgram(gl, Arrays.asList(shaders));
-    }
-
-    private void linkPrograms(GL3 gl, int handle) {
-        gl.glLinkProgram(handle);
-        if(!isLinked(gl, handle)) {
-            printLinkerError(gl, handle);
-        }
-    }
-
-    private void printLinkerError(GL3 gl, int handle) {
-        IntBuffer logLengthBuffer = Buffers.newDirectIntBuffer(1);
-        gl.glGetProgramiv(handle, GL3.GL_INFO_LOG_LENGTH, logLengthBuffer);
-        int length = logLengthBuffer.get();
-
-        ByteBuffer data = Buffers.newDirectByteBuffer(length);
-        gl.glGetProgramInfoLog(handle, length, null, data);
-        byte[] bytes = new byte[length];
-        data.get(bytes);
-        System.out.println(new String(bytes));
-    }
-
-    private boolean isLinked(GL3 gl, int handle) {
-        IntBuffer status = Buffers.newDirectIntBuffer(1);
-        gl.glGetProgramiv(handle, GL3.GL_INFO_LOG_LENGTH, status);
-        return status.get() == GL3.GL_TRUE;
-    }
-
-    private Map<String, Integer> getUniforms(GL3 gl) {
-        Map<String, Integer> uniforms = new HashMap<>();
-        int handle = getHandle();
-        IntBuffer countBuffer = IntBuffer.allocate(1);
-        gl.glGetProgramiv(handle, GL3.GL_ACTIVE_UNIFORMS, countBuffer);
-        int limit = countBuffer.get();
-        for(int i = 0; i < limit; i++) {
-            IntBuffer length = IntBuffer.allocate(1);
-            ByteBuffer buffer = ByteBuffer.allocate(100);
-            gl.glGetActiveUniformName(handle, i, 100, length, buffer);
-            byte[] bytearr = new byte[buffer.remaining()];
-            buffer.get(bytearr);
-            String name = new String(bytearr).trim();
-            uniforms.put(name, gl.glGetUniformLocation(handle, name));
-        }
-        return uniforms;
-    }
-
-    private Map<String, Integer> getAttrbutes(GL3 gl) {
-        Map<String, Integer> uniforms = new HashMap<>();
-        int handle = getHandle();
-        IntBuffer countBuffer = IntBuffer.allocate(1);
-        gl.glGetProgramiv(handle, GL3.GL_ACTIVE_ATTRIBUTES, countBuffer);
-        int limit = countBuffer.get();
-        for(int i = 0; i < limit; i++) {
-            IntBuffer length = IntBuffer.allocate(1);
-            IntBuffer size = IntBuffer.allocate(1);
-            ByteBuffer buffer = ByteBuffer.allocate(100);
-            gl.glGetActiveAttrib(handle, i, 100, length, IntBuffer.allocate(1), IntBuffer.allocate(1), buffer);
-            byte[] bytearr = new byte[buffer.remaining()];
-            buffer.get(bytearr);
-            String name = new String(bytearr).trim();
-            uniforms.put(name, gl.glGetAttribLocation(handle, name));
-        }
-        return uniforms;
+    void addUniform(String name, Integer location) {
+        uniforms.put(name, location);
     }
 }
