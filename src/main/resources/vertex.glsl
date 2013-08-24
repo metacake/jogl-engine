fdcfd24ba4711690da07f5987e7d9b8fcd079086
Ryan Plessner <rpless@ccs.neu.edu>
2013-08-24 14:40:58 -0400

    classic, merged in some crap that shouldn't have been merged

--- src/main/resources/vertex.glsl
+++ src/main/resources/vertex.glsl
@@ -1,20 +1,5 @@
 #version 150
 in vec4 position;
-
-uniform float zNear;
-uniform float zFar;
-uniform float frustumScale;
-
 void main() {
-    vec4 camerPos = psotion + vec4(0.25f, 0.5f, 0, 0);
-    vec4 clipPos;
-
-    clipPos.xy = cameraPos.xy * frustumScale;
-
-    clipPos.z = cameraPos.z * (zNear + zFar) / (zNear - zFar);
-    clipPos.z += 2 * zNear * zFar / (zNear - zFar);
-
-    clipPos.w = -cameraPos.z;
-
-    gl_Position = clipPos;
+   gl_Position = position;
 }
